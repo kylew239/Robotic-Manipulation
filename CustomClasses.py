698e34c21c38d17fe6104c10ddc5bc9e0321fc5a
Kyle <kylewang239@gmail.com>
2023-11-13 03:01:29 -0600

    Worked on Milestone 1: -Created NextState and helper functions -AFAIK, the position is working
    TODO:
    -Check matrices math
    -Need to test arms and wheel rotations
    -Test in CoppSim Scene 6

--- CustomClasses.py
+++ CustomClasses.py
@@ -1,11 +1,12 @@
-class Point:
+class Pose:
     def __init__(self,
                  x: float = 0.0,
                  y: float = 0.0,
-                 z: float = 0.0):
+                 phi: float = 0.0):
         self.x = x
         self.y = y
-        self.z = z
+        self.phi = phi
+
 
 class ArmAng:
     def __init__(self,
@@ -20,6 +21,7 @@ class ArmAng:
         self.th4 = th4
         self.th5 = th5
 
+
 class WheelAng:
     def __init__(self,
                  th1: float = 0.0,
@@ -31,38 +33,62 @@ class WheelAng:
         self.th3 = th3
         self.th4 = th4
 
-class Config:
+
+class RobotState:
     def __init__(self,
-                 pos: Point,
-                 joint: ArmAng,
-                 wheel: WheelAng):
-        self.pos = chassis
+                 pos: Pose = Pose(x=0.0,
+                                  y=0.0,
+                                  phi=0.0),
+                 joint: ArmAng = ArmAng(0.0,
+                                        0.0,
+                                        0.0,
+                                        0.0,
+                                        0.0),
+                 wheel: WheelAng = WheelAng(0.0,
+                                            0.0,
+                                            0.0,
+                                            0.0)):
+        self.pos = pos
         self.joint = joint
         self.wheel = wheel
 
+
 class wheelVel:
     def __init__(self,
-                 u1: float,
-                 u2: float,
-                 u3: float,
-                 u4: float):
-        self.u1 = u1
-        self.u2 = u2
-        self.u3 = u3
-        self.u4 = u4
+                 th1: float = 0.0,
+                 th2: float = 0.0,
+                 th3: float = 0.0,
+                 th4: float = 0.0):
+        self.th1 = th1
+        self.th2 = th2
+        self.th3 = th3
+        self.th4 = th4
+
 
 class jointVel:
     def __init__(self,
-                 th1: float,
-                 th2: float,
-                 th3: float,
-                 th4: float,
-                 th5: float):
+                 th1: float = 0.0,
+                 th2: float = 0.0,
+                 th3: float = 0.0,
+                 th4: float = 0.0,
+                 th5: float = 0.0):
         self.th1 = th1
         self.th2 = th2
         self.th3 = th3
         self.th4 = th4
         self.th5 = th5
 
-class Controls:
-    pass
+
+class Velocities:
+    def __init__(self,
+                 wheel: wheelVel = wheelVel(0.0,
+                                            0.0,
+                                            0.0,
+                                            0.0),
+                 joint: jointVel = jointVel(0.0,
+                                            0.0,
+                                            0.0,
+                                            0.0,
+                                            0.0)):
+        self.wheel = wheel
+        self.joint = joint
--- main.py
+++ main.py
@@ -1,6 +1,135 @@
 import numpy as np
 import modern_robotics as mr
 from CustomClasses import *
+import math
 
-def NextState():
-    pass
+# kinematics of youBot
+l = 0.235
+w = 0.15
+r = 0.0475
+Tb0 = [[1, 0, 0, 0.1662],
+       [0, 1, 0, 0],
+       [0, 0, 1, 0.0026],
+       [0, 0, 0, 1]]
+M0e = [[1, 0, 0, 0.033],
+       [0, 1, 0, 0],
+       [0, 0, 1, 0.6546],
+       [0, 0, 0, 1]]
+
+# Screw Axes for home config
+B1 = [0,  0, 1,       0, 0.033, 0]
+B2 = [0, -1, 0, -0.5076,     0, 0]
+B3 = [0, -1, 0, -0.3526,     0, 0]
+B4 = [0, -1, 0, -0.2176,     0, 0]
+B5 = [0,  0, 1,       0,     0, 0]
+
+# H for mecanum drive
+mecanum = np.matrix([[-l-w, 1, -1],
+                     [l+w, 1,  1],
+                     [l+w, 1, -1],
+                     [-l-w, 1,  1]])
+
+
+def NextState(currentConf: RobotState,
+              currentVels: Velocities,
+              dt: float,
+              maxAngVel: float) -> RobotState:
+    # Do first-order euler step for joint angles
+    currentConf.joint = firstEulerStep(currentConf.joint,
+                                       currentVels.joint,
+                                       dt,
+                                       maxAngVel)
+
+    # Do first-order euler step for wheel angles
+    currentConf.wheel = firstEulerStep(currentConf.wheel,
+                                       currentVels.wheel,
+                                       dt,
+                                       maxAngVel)
+    
+    # Update chassis position
+    currentConf.pos = calculateChassisPose(currentConf.pos,
+                                           currentVels.wheel,
+                                           dt,
+                                           maxAngVel)
+
+    return currentConf
+
+
+def firstEulerStep(item: ArmAng or WheelAng,
+                   itemVels: jointVel or wheelVel,
+                   dt: float,
+                   maxAngVel: float):
+    """Calculate First euler step for each value in object.
+
+    :param item: Object to calculate
+    :param dt: time step
+    :param maxAngVel: Maximum angular velocity
+    :return: Object with Updated Values
+    """
+    # Iterate through each value in the object
+    for key in vars(item):
+        # If velocity is under the limit, use the velocity
+        if abs(getattr(itemVels, key)) < maxAngVel:
+            setattr(item,
+                    key,
+                    getattr(item, key) + getattr(itemVels, key)*dt)
+        # Else use velocity limit
+        else:
+            setattr(item,
+                    key,
+                    getattr(item, key) + np.sign(getattr(itemVels, key))*maxAngVel)
+    return item
+
+
+def calculateTsb(phi: float,
+                 x: float,
+                 y: float):
+    """Calculate Tsb given the q vector
+
+    :param phi: Angle of rotation about the z axis of {s}
+    :param x: x distance to {s}
+    :param y: y distance to {s}
+    :return: Tsb
+    """
+    return [[math.cos(phi), -math.sin(phi), 0, x],
+            [math.sin(phi),  math.cos(phi), 0, y],
+            [0,              0, 1, 0.0963],
+            [0,              0, 0, 1]]
+
+
+def calculateChassisPose(pose: Pose,
+                         wheelVels: wheelVel,
+                         dt: float,
+                         maxAngVel: float):
+    """Update position of chassis given the current position, wheel velocities, and dt
+
+    :param pose: Current position of the robot
+    :param wheelVels: Angular velocities of each wheel
+    :param dt: time step
+    :param maxAngVel: Maximum angular velocity
+    :return: updated position
+    """
+    u = np.clip(np.array([[wheelVels.th1, wheelVels.th2, wheelVels.th3, wheelVels.th4]]),
+                -maxAngVel,
+                maxAngVel)
+    #TODO: Clean up comments and check if math is right
+    # chassisVels = np.divide(u.T, mecanum) * r
+
+    chassisVels = np.matmul((u*r), np.linalg.pinv(mecanum).T)
+    pose.phi += chassisVels[0, 0] * dt
+    pose.x += chassisVels[0, 1] * dt
+    pose.y += chassisVels[0, 2] * dt
+    # print("1,", mecanum.shape)
+    # print("2,", chassisVels.shape)
+    u_calc = np.matmul(mecanum, chassisVels.T)
+    # print("u_calculated: ", chassisVels.shape)
+    return pose
+
+if __name__ == "__main__":
+    robot = RobotState()
+    velocities = Velocities(wheel=wheelVel(-10, 10, 10, -10))
+    t = 0
+    while t < 1000:
+        robot = NextState(robot, velocities, 0.01, 50)
+        t += 1
+    print(f"X: {robot.pos.x}, y: {robot.pos.y}, phi: {robot.pos.phi}")
